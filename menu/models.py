f8b5cfafe39cdbb619662bd994707f5587a6c2e5
Vimal Ram <ramvimal722@gmail.com>
2025-12-22 17:04:54 +0530

    In orders app two model created (1.Order | 2. OrderItem)

--- menu/models.py
+++ menu/models.py
@@ -15,4 +15,4 @@ class MenuItem(models.Model):
     price = models.DecimalField(max_digits=6, decimal_places=2)
 
     def __str__(self):
-        return self.name
+        return self.namex   
--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+class Order(models.Model):
+    create_at = models.DateTimeField(auto_now_add=True)
+    is_paid = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"Order #{self.id}"
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order,on_delete=models.CASCADE)
+    food_item = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=8,decimal_places=2)
+    quantity = models.PositiveIntegerField(default=1)
+
+    def get_total(self):
+        return self.price * self.quantity
