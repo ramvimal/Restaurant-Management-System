f4515396b970ec49bccd4b2a54b5558bb3d0de6f
Vimal Ram <ramvimal722@gmail.com>
2025-12-22 19:06:10 +0530

    Changed Urls and created views.py file and menu.html file

--- menu/models.py
+++ menu/models.py
@@ -13,6 +13,7 @@ class MenuItem(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
+    available = models.BooleanField(default=True)
 
     def __str__(self):
-        return self.namex   
+        return self.name 
--- menu/views.py
+++ menu/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from .models import Category , MenuItem
 
 # Create your views here.
+def menu_list(request):
+    categories = Category.objects.all()
+
+    menu_data = []
+
+    for category in categories:
+        items = MenuItem.objects.filter(category=category,available=True)
+
+        menu_data.append(
+            {
+                'category':category,
+                'items':items,
+            }
+        )
+
+    return render(request,"menu/menu.html",{"menu_data":menu_data})
